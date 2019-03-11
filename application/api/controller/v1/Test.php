a1997711fddeb79f8531a6563e8fada71df5f056
Derek <463645977@qq.com>
2019-03-11 22:42:06 +0800

    添加测试接口

---
+++ application/api/controller/v1/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 2019/3/11
+ * Time: 10:39 PM
+ */
+
+namespace app\api\controller\v1;
+
+
+class Test
+{
+    public function hello()
+    {
+        return 'hello,derek';
+    }
+}
