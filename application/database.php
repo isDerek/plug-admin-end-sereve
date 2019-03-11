75f9d2a9a79683486fcc68eca8ff75dab9309fe7
Derek <463645977@qq.com>
2019-03-11 11:48:50 +0800

    修改了数据库到 Centos7 数据库上

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.112.195.182',
     // 数据库名
-    'database'        => 'plug-back-end',
+    'database'        => 'plug',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => '12345678',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
